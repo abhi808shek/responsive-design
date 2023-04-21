44a44693f4444a6be350351953f83fd718382eb1
abhi808shek <abhi808gupta@gmail.com>
2023-04-21 13:24:57 +0530

    Api Integration

--- src/redux/actionCreators/authActionCreator.js
+++ src/redux/actionCreators/authActionCreator.js
@@ -2,16 +2,110 @@ import axios from "axios";
 
 // CHECKING USER pr Email EXIST OR NOT
 
-export const checkingUserExist = (emailId) => async (dispatch) => {
+const config =  {
+  headers: {
+    "Accept-Language": "en",
+  },
+}
+
+
+export const saveUserSignupData = (data) => async(dispatch) => {
+  console.log("DATA",data);
+  const datalist = {
+    datetime:data.datetime,
+    profileType: data.profileType,
+    uemail: data.uemail,
+  }
+  console.log("datalist",datalist);
+  try {
+    const result = await axios.put(
+      `http://3.233.82.34:8080/api/user/registerotp`,datalist,
+      {
+        headers: {
+          "Accept-Language": "en",
+        },
+      }
+    );
+    console.log("result", result);
+  dispatch({
+  type: "SET_USER_SIGNUP_DATA",
+  payload:data,
+})
+return result.status
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+
+
+export const matchingSignupOtp = (mailId,otp) => async(dispatch) => {
+  console.log("mailId",mailId);
+  console.log("otp",otp);
+  try {
+    const result = await axios.get(
+      `http://3.233.82.34:8080/api/user/otp/${mailId}/${otp}`,
+      {
+        headers: {
+          "Accept-Language": "en",
+        },
+      }
+    );
+    console.log("resultsdfgsd", result);
+    dispatch({
+      type: "MATCHING_SIGNUP_OTP",
+      // payload:data,
+    });
+    return result.data.status
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+
+
+
+
+
+
+export const checkingUserExist = (emailId) => async(dispatch) => {
   try {
     const userExist = await axios.get(
-      `http://3.233.82.34:8080/api/user/usersbyemail/${emailId}`,{headers: {
-        "Accept-Language": "en"
-  }}
+      `http://3.233.82.34:8080/api/user/usersbyemail/${emailId}`,
+      {
+        headers: {
+          "Accept-Language": "en",
+        },
+      }
     );
+    console.log("userExist", userExist.data);
     dispatch({
       type: "USER_EXIST_OR_NOT",
       payload: userExist.data,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// CALLING PUT API FOR SENDING OTP
+
+export const sendingMailForOtp = (data) => async (dispatch) => {
+  try {
+    const mailSend = await axios.put(
+      `http://3.233.82.34:8080/api/user/otp`,
+      data,
+      {
+        headers: {
+          "Accept-Language":"en",
+        },
+      }
+    );
+
+    console.log("mailSend", mailSend.data);
+    dispatch({
+      type: "SENDING_MAIL_FOR_OTP",
+      payload: mailSend.data,
     });
   } catch (error) {
     console.log(error.message);
--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,11 +1,28 @@
 const initialState = {
-    emailExist:{}
+  signupData:{},
+    emailExist:{},
+    mailSended:{}
   };
   
   const authReducer = (state = initialState, action) => {
     switch (action.type) {
+      // STORING DATA WHILE SIGNUP 
+      case "SET_USER_SIGNUP_DATA":
+        return {
+          ...state,
+          signupData:action.payload,
+        };
+
+
+        case "MATCHING_SIGNUP_OTP":
+          return {
+            ...state,
+            // signupDataList: [...state.signupDataList,action.payload],
+          };
       case "USER_EXIST_OR_NOT":
         return { ...state,emailExist:action.payload}
+        case "SENDING_MAIL_FOR_OTP":
+          return { ...state,mailSended:action.payload}
       default:
         return state;
     }
