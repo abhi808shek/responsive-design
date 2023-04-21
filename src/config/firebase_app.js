a381fd84ce561a4cc03ecb11fb7a14337e6ae78f
abhi808shek <abhi808gupta@gmail.com>
2023-04-21 19:23:44 +0530

    COnflict Resolve

--- src/config/firebase_app.js
+++ src/config/firebase_app.js
@@ -1,45 +1,35 @@
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken } from 'firebase/messaging';
 
 
-
-import firebase from 'firebase/app';
-import 'firebase/messaging';
-import 'firebase/auth';
-import 'firebase/firestore';
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
-
 const firebaseConfig = {
-    apiKey: "AIzaSyDyUX4gM6ZfbKC7F2Fa-ILNOMuPi4kiR04",
-    authDomain: "notificaton-token.firebaseapp.com",
-    projectId: "notificaton-token",
-    storageBucket: "notificaton-token.appspot.com",
-    messagingSenderId: "288783732612",
-    appId: "1:288783732612:web:8144c819211bb720b217d1",
-    measurementId: "G-DSFPXMN3DX"
-  };
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id",
+  measurementId: "your-measurement-id"
+};
 
 // Initialize Firebase
-if (!firebase.apps.length) {
-  console.log("Hellloooo");
-  firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+
+export function getFCMToken() {
+  getToken(messaging, { vapidKey: "BEZbUPDv237uhVZ40nWS5dmHoNLKcJhrJRVIvhjAWHFX-DSWRHElA7eQpUB9A0ZqBa5usIlhyUpK8vgYACXCsgs" })
+    .then((currentToken) => {
+      if (currentToken) {
+        console.log('FCM token:', currentToken);
+        // send the token to your server to use for sending push notifications
+      } else {
+        console.log('No registration token available.');
+      }
+    })
+    .catch((error) => {
+      console.log('An error occurred while retrieving the FCM token:', error);
+    });
 }
-
-
- export function getFCMToken() {
-    firebase.messaging().getToken({ vapidKey: "BEZbUPDv237uhVZ40nWS5dmHoNLKcJhrJRVIvhjAWHFX-DSWRHElA7eQpUB9A0ZqBa5usIlhyUpK8vgYACXCsgs"})
-      .then((currentToken) => {
-        if (currentToken) {
-          console.log('FCM token:', currentToken);
-          // send the token to your server to use for sending push notifications
-        } else {
-          console.log('No registration token available.');
-        }
-      })
-      .catch((error) => {
-        console.log('An error occurred while retrieving the FCM token:', error);
-      });
-  }
-  
-
-
-
