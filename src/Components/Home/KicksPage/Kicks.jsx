c2896116899e90dd5aea5a4ebf7bf8609caaa938
Akpro865 <devak8653gmail.com>
2023-04-23 20:06:19 +0530

    kicks page fix

--- src/Components/Home/KicksPage/Kicks.jsx
+++ src/Components/Home/KicksPage/Kicks.jsx
@@ -9,6 +9,15 @@ import v2 from '../../../Assets/Videos/v2.mp4';
 import v3 from '../../../Assets/Videos/v3.mp4';
 import v4 from '../../../Assets/Videos/v4.mp4';
 import v5 from '../../../Assets/Videos/v5.mp4';
+import { HiPlus, HiSearch } from 'react-icons/hi'
+import { BsMusicNoteList } from 'react-icons/bs'
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa'
+import mute from '../../../assets/images/mute.png'
+import Messages from '../../../assets/images/Messages.png'
+import like from '../../../assets/images/KicksBeforeLike.png'
+import share from '../../../assets/images/share.png'
+import './kicks.css'
+
 const Kicks = () => {
   const data = [
     { title: "Following" },
@@ -17,15 +26,16 @@ const Kicks = () => {
   ];
 
   const dataList = [
-    { title: "mute" },
-    { title: "25 likes" },
-    { title: "3 comments" },
-    { title: "share" },
+    { title: "mute", img: mute  },
+    { title: "25 likes", img: like },
+    { title: "3 comments", img: Messages },
+    { title: "share", img: share },
   ];
   const dispatch = useDispatch();
   const { kicksType } = useSelector((state) => state.userReducer);
+
   return (
-    <div className={`w-full grid grid-cols-3 h-[88vh] bg-[url(${events})] z-10`}>
+    <div className={`w-full grid grid-cols-3 h-[90vh] bg-[url(${events})] z-10`}>
       <section className="flex items-center justify-center bg-black">
         <div className="w-[80%] flex flex-col items-center">
           <div className="flex flex-col gap-3">
@@ -43,39 +53,22 @@ const Kicks = () => {
                 {elem.title}
               </p>
             ))}
-            <div className="flex gap-2">
-              <img
-                src="./images/groups.png"
-                alt=""
-                className="w-[40px] cursor-pointer"
-              />
-              <img
-                src="./images/groups.png"
-                alt=""
-                className="w-[40px] cursor-pointer"
-              />
-              <img
-                src="./images/groups.png"
-                alt=""
-                className="w-[40px] cursor-pointer"
-              />
+            <div className="flex justify-evenly gap-2">
+              <HiSearch className='w-8 p-0.5 h-8 bg-[#6e6f6f] text-white rounded-full'/>
+              <HiPlus className='w-8 h-8 p-0.5 bg-[#6e6f6f] rounded-full text-white'/>
             </div>
           </div>
         </div>
 
         {/*Left Arrow Button Section */}
         <div className="w-[20%]">
-          <img
-            src="./images/groups.png"
-            alt=""
-            className="w-[40px] cursor-pointer"
-          />
+          <FaArrowAltCircleLeft className="w-9 h-9 text-white cursor-pointer" />
         </div>
       </section>
 
       {/* Reels Section */}
 
-      <section className="overflow-y-scroll">
+      <section className="overflow-y-scroll hideScroll">
         <VideoComponent dataList={dataList} />
         <VideoComponent dataList={dataList} />
         {/* <section className="mt-8 h-[90vh] w-[30rem] relative left-4 rounded-[20px] flex justify-center items-center">
@@ -117,12 +110,8 @@ const Kicks = () => {
             <div className="w-[20%] font-bold py-0.5 bg-white text-gray-500 flex items-center justify-center rounded-lg">
               @music
             </div>
-            <img
-              src="./images/groups.png"
-              alt=""
-              className="w-[30px] cursor-pointer"
-            />
-            <p className="flex items-center font-bold">
+            <BsMusicNoteList className="w-7 h-7 text-white cursor-pointer"/>
+            <p className="flex items-center">
               maayadari maayadari andamaa
             </p>
           </div>
@@ -130,11 +119,7 @@ const Kicks = () => {
 
         {/*Right Arrow Button */}
         <div className="w-[20%]">
-          <img
-            src="./images/groups.png"
-            alt=""
-            className="w-[40px] cursor-pointer"
-          />
+          <FaArrowAltCircleRight className="w-9 h-9 text-white cursor-pointer" />
         </div>
       </section>
     </div >
--- src/Components/Home/KicksPage/VideosComponent/VideoComponent.jsx
+++ src/Components/Home/KicksPage/VideosComponent/VideoComponent.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactPlayer from "react-player";
 import v2 from '../../../../Assets/Videos/v2.mp4';
+import { BsThreeDotsVertical } from 'react-icons/bs'
+import eye from '../../../../Assets/Images/Eye icon.png'
 
 const VideoComponent = ({ dataList, imgName }) => {
 
   return (
-    <div className="mt-5 mb-4 border-2 border-red-600 rounded-[20px] p-3">
-      <div className="flex h-[50px] px-4 items-center py-2 relative">
+    <div className="relative h-full">
+
+      <section className="absolute overflow-hidden top-0 left-0 h-full w-full">
+         <video className="h-auto w-full" loop={true} autoPlay="autoplay" controls>
+           <source src={v2} type="video/mp4" />
+         </video>
+      </section>
+      <div className="flex h-[50px] z-20 px-1 items-center py-2 relative">
         {/* <ReactPlayer url='https://www.youtube.com/watch?v=vNeN13EQbqk' /> */}
 
 
@@ -16,48 +24,43 @@ const VideoComponent = ({ dataList, imgName }) => {
           <img
             src="./images/events.jpg"
             alt=""
-            className="w-[45px] h-[45px] rounded-full"
+            className="w-[45px] h-[45px] rounded-full object-cover"
           />
         </div>
 
         {/* For Username and time when he created the video and views */}
 
-        <div className=" flex flex-1 flex-col justify-center ml-2">
+        <div className=" flex flex-1 flex-col text-white justify-center ml-2">
           <div className="flex items-center gap-2">
-            <h1 className="font-semibold text-black ">Elisa K</h1>
-            <p className="text-[10px] text-black font-bold">5 hours ago</p>
+            <h1 className="font-semibold ">Elisa K</h1>
+            <p className="text-[10px] font-medium">5 hours ago</p>
           </div>
 
           <div className="flex gap-2 items-center">
             <img
-              src="./images/groups.png"
+              src={eye}
               alt=""
-              className="w-[10px] h-[10px]"
+              className="w-[15px] h-[15px]"
             />
-            <p className="text-[10px] font-bold text-black">24 Views</p>
+            <p className="text-[10px]">24 Views</p>
           </div>
         </div>
 
-        <div className="flex gap-2 items-center cursor-pointer">
-          <img src="./images/groups.png" alt="" className="w-[30px] h-[30px]" />
+        <div className="flex items-center cursor-pointer">
+          <BsThreeDotsVertical className="w-[27px] h-[27px] text-white" />
         </div>
       </div>
 
       {/* For creating mute, likes, comments and share */}
 
-      <div className="flex flex-col items-end justify-end w-full h-[80vh] px-3">
-        <section className="relative right-20 top-[45vh]">
-        <video className="h-[80%] w-full" loop={true} autoPlay="autoplay" controls>
-          <source src={v2} type="video/mp4" />
-        </video>
-        </section>
+      <div className="flex flex-col items-end justify-end w-full h-[78vh] px-3">        
         {dataList.map((elem) => (
           <div
             key={elem.title}
-            className="flex items-end gap-2 font-bold flex-col"
+            className="flex z-20 items-end gap-2 font-semibold flex-col"
           >
             <img
-              src="./images/groups.png"
+              src={elem.img}
               alt=""
               className="w-[30px] cursor-pointer"
             />
