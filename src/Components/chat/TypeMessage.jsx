5befd05e3372f376b1c0d69e44850cab79710793
amal_436 <amalshakya436@gmail.com>
2023-05-19 10:59:09 +0530

    chatMessage and chatPages updated

--- src/Components/chat/TypeMessage.jsx
+++ src/Components/chat/TypeMessage.jsx
@@ -6,18 +6,19 @@ import { MdSend } from 'react-icons/md';
 import { CgSmileMouthOpen } from 'react-icons/cg';
 import { useState } from 'react';
 
-const TypeMessage = ({ placeholder, sendMessage}) => {
+const TypeMessage = ({ placeholder, sendMessage,}) => {
+
   const [state, setState] = useState({})
-  const { text} = state
+  const { text } = state
   return (
     <div className='flex items-center'>
-        <BsEmojiSmile size={32} className='me-2 cursor-pointer'/>
-        <input id='sendFile' type='file' className='hidden' />
-        <label htmlFor='sendFile'><RiImageFill size={32} className='cursor-pointer'/></label>
-        <div className='mx-6 w-full'>
-            <SearchComponent handleInputChange={(e) => setState({...state, text: e})} bgColor={'#e4e7ec'} placeholder={placeholder} icon />
-        </div>
-        <MdSend className='cursor-pointer' size={34} onClick={() => sendMessage(text)}/>
+      <BsEmojiSmile size={32} className='me-2 cursor-pointer' />
+      <input id='sendFile' type='file' className='hidden' />
+      <label htmlFor='sendFile'><RiImageFill size={32} className='cursor-pointer' /></label>
+      <div className='mx-6 w-full'>
+        <SearchComponent handleInputChange={(e) => setState({ ...state, text: e })} bgColor={'#e4e7ec'} placeholder={placeholder} icon />
+      </div>
+      <MdSend className='cursor-pointer' size={34} onClick={() => sendMessage(text)} />
     </div>
   )
 }
